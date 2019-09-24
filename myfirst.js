421a2d55335d786b4fe60c2f57bc6b0d3380ae4a
goncalo <goncalof@msn.com>
2019-09-24 15:11:47 +0100

    update js

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,4 @@
-var var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080); = require('http');
+var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
